be8e94b0d3171e3d85689a1315f50fddfc7ce25c
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2023-05-02 01:11:00 -0400

    real

---
+++ fakeDeafen.ts
@@ -0,0 +1,20 @@
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { Toasts } from "@webpack/common";
+
+export default definePlugin({
+    name: "FakeDeafen",
+    description: "make ppl think u ae deafen!!!!!!!!!!",
+    authors: [Devs.Ven],
+
+    async start() {
+      activate();
+    },
+
+    stop() {
+    },
+
+    activate() {
+        console.log('you're gullible');
+    }
+});
