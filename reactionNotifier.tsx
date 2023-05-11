f935c40f30a1b98e2a55b900fede7001210a5816
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2023-05-11 19:33:42 -0400

    still wipo

--- reactionNotifier.tsx
+++ reactionNotifier.tsx
@@ -19,9 +19,11 @@
 import { showNotification } from "@api/Notifications";
 import { definePluginSettings } from "@api/Settings";
 import { Logger } from "@utils/Logger";
+import { Queue } from "@utils/Queue";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { MessageStore, NavigationRouter, Text, UserStore } from "@webpack/common";
+import { FluxDispatcher, MessageStore, NavigationRouter, RestAPI, Text, UserStore } from "@webpack/common";
+import { Message } from "discord-types/general";
 
 const settings = definePluginSettings({
     reactAdd: { description: "Notify for Reaction added", type: OptionType.BOOLEAN, default: true, },
@@ -31,9 +33,9 @@ const settings = definePluginSettings({
     ignoreBots: { description: "Ignore bot reactions", type: OptionType.BOOLEAN, default: true, },
 });
 
-const bruh = new Logger("ReactionNotifier");
+const bruh = new Logger("ReactionNotifier"); /* remove when finished */
 
-const isDM = (r: { guild_id: string; }) => !r.guild_id;
+const messageFetchQueue = new Queue();
 
 export default definePlugin({
     name: "ReactionNotifier",
@@ -47,47 +49,95 @@ export default definePlugin({
             if (!settings.store.reactAdd) return;
             const currentUserId = UserStore.getCurrentUser().id;
             if (r.userId === currentUserId || r.messageAuthorId !== currentUserId) return;
-            bruh.info(`Reaction added: ${r}`);
+            bruh.info(`Raw Reaction Add: ${r}`);
             const user = UserStore.getUser(r.userId);
+            if (user.bot && settings.store.ignoreBots) return;
             // TODO: fetch unloaded messages or else get undefined content error lol
-            const msg = MessageStore.getMessage(r.channelId, r.messageId);
-            const emoji = `https://cdn.discordapp.com/emojis/${r.emoji.id}.png?size=128`;
-            /*if (isDM(r)) {*/
-            showNotification({
-                title: `${user.username} reacted with ${r.emoji.name}`,
-                body: msg.content ? msg.content : "",
-                richBody:
-                    <>
-                        {r.emoji.id && <img className="vc-rn-emoji" src={emoji} width={24} height={24} alt="" />}
-                        <Text variant="text-md/normal">{msg.content}</Text>
-                    </>,
-                icon: user.getAvatarURL(),
-                onClick() {
-                    const link = "/channels/" + (r.guild_id ? r.guild_id : "@me") + "/" + msg.channel_id + "/" + msg.id;
-                    // focus discord
-                    window.focus();
-                    NavigationRouter.transitionTo(link);
+            let msg = messageCache.get(r.messageId)?.message;
+            let msgId = r.messageId;
+            if (!msg) {
+                msg ??= MessageStore.getMessage(r.channelId, msgId);
+                if (msg) {
+                    messageCache.set(msgId, { message: msg, fetched: true });
+                } else {
+                    messageFetchQueue.push(() => fetchMessage(r.channelId, msgId)
+                        .then(m => {
+                            if (!m) return;
+                            FluxDispatcher.dispatch({
+                                type: "MESSAGE_UPDATE",
+                                message: msg
+                            });
+                            const emoji = `https://cdn.discordapp.com/emojis/${r.emoji.id}.png?size=128`;
+                            showNotification({
+                                title: `${user.username} reacted with ${r.emoji.name}`,
+                                body: m.content,
+                                richBody:
+                                    <>
+                                        {r.emoji.id && <img className="vc-rn-emoji" src={emoji} width={24} height={24} alt="" />}
+                                        <Text variant="text-md/normal">{m.content}</Text>
+                                    </>,
+                                icon: user.getAvatarURL(),
+                                onClick() {
+                                    const link = "/channels/" + (r.guildId ? r.guildId : "@me") + "/" + r.channelId + "/" + r.messageId;
+                                    // focus discord
+                                    window.focus();
+                                    NavigationRouter.transitionTo(link);
+                                }
+                            });
+                        })
+                    );
+
                 }
-            });
-            return;
-            /*}*/
-            if (settings.store.ignoreBots && r.member.user.bot) return;
-            bruh.info(`Reaction added: ${r}`);
+            }
         },
         MESSAGE_REACTION_REMOVE(r) {
             if (!settings.store.reactRemove) return;
-            bruh.info(`Reaction removed: ${r}`);
+            // TODO
         },
         BURST_REACTION_ADD(r) {
             if (!settings.store.burstAdd) return;
-            bruh.info(`Burst added: ${r}`);
+            // TODO
         },
         BURST_REACTION_REMOVE(r) {
             if (!settings.store.burstRemove) return;
-            bruh.info(`Burst removed: ${JSON.stringify(r)}`);
+            // TODO
         }
     },
 
     start() { },
     stop() { },
 });
+
+
+const messageCache = new Map<string, {
+    message?: Message;
+    fetched: boolean;
+}>();
+
+async function fetchMessage(channelID: string, messageID: string) {
+    const cached = messageCache.get(messageID);
+    if (cached) return cached.message;
+
+    messageCache.set(messageID, { fetched: false });
+
+    const res = await RestAPI.get({
+        url: `/channels/${channelID}/messages`,
+        query: {
+            limit: 1,
+            around: messageID
+        },
+        retries: 2
+    }).catch(() => null);
+
+    const msg = res?.body?.[0];
+    if (!msg) return;
+
+    const message: Message = MessageStore.getMessages(msg.channel_id).receiveMessage(msg).get(msg.id);
+
+    messageCache.set(message.id, {
+        message,
+        fetched: true
+    });
+
+    return message;
+}
